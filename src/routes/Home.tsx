b0a2e108258fb51aa9d3d051abf6a0c623e7215a
pwh9882 <pwh9882@gmail.com>
2023-05-04 22:20:13 +0900

    간단한 화면 글리치 수정(이제 데이터 길이에 따라 배경이 따라옴.)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,13 +8,13 @@ const Home = () => {
       <div>
         <h1>Home</h1>
       </div>
-      <Link to={"/station-detail?stationId=100100015"}>
+      <Link to={{pathname:"/station-detail"}} state={{stationUid: "08110"}}>
         <div className="item station-dumy">
           <h2>국민대학교 앞</h2>
           <h3>롯데아파트 방면</h3>
         </div>
       </Link>
-      <Link to={"/bus-route-detail"}>
+      <Link to={"/bus-route-detail"} state={{busRouteId:"100100015"}}>
         <div className="item bus-dummy">
           <h2>110B 국민대</h2>
         </div>
