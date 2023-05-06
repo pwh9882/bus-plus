13147121e3f2ff506d5fe04d5187f126938460c0
pwh9882 <pwh9882@gmail.com>
2023-05-06 14:31:10 +0900

    search 기능 구현을 위한 api준비

--- src/busApi/busRouteInfo/getBusRouteList.tsx
+++ src/busApi/busRouteInfo/getBusRouteList.tsx
@@ -1,9 +1,9 @@
 import { parseXml } from "xmlParser";
 
-export const getStationByName = async (strSrch: string): Promise<Array<any> > => {
+export const getBusRouteList = async (strSrch: string): Promise<Array<any> > => {
   
     const api =
-    `api/rest/stationinfo/getBusRouteList?serviceKey=${process.env.REACT_APP_BUS_API_KEY}&strSrch=${strSrch}`;
+    `api/rest/busRouteInfo/getBusRouteList?serviceKey=${process.env.REACT_APP_BUS_API_KEY}&strSrch=${strSrch}`;
     const xmlRowText = await (await fetch(api, {})).text();
     // console.log(xmlRowText);
     let resultArray:any = (await parseXml(xmlRowText)).ServiceResult.msgBody[0]?.itemList;
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import "css/Search.css";
+import { useState } from "react";
 
 const Search = () => {
   const navigate = useNavigate();
+  const [option, setOption] = useState("bus");
+  const [searchTerm, setSearchTerm] = useState("");
   const onCencelClicked = () => {
     navigate("/");
   };
+  
   return (
     <>
       <div className="search-bar">
@@ -13,6 +17,22 @@ const Search = () => {
         <input type="text" placeholder="버스 또는 정류장 검색" />
       </div>
       <h1>Search</h1>
+      <div className="searchOption">
+        <div>버스 검색</div>
+        <div>정류장 검색</div>
+      </div>
+      <ul id="search-options" className="options">   
+        <li className="option">
+          <button className="search-bus-route" aria-pressed="true" aria-selected>
+            버스 이름으로 검색
+          </button>
+        </li>
+        <li className="option">
+          <button className="search-station" aria-pressed="false">
+            정류장 이름으로 검색
+          </button>
+        </li>
+        </ul>
     </>
   );
 };
